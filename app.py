c496c051883edf1cabaf2d13917ecd8eb28fc782
Callmenicky <70570497+Callmenicky@users.noreply.github.com>
2021-11-11 23:29:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def basicpredmethod():
     molecule_list = [data1]#insert name of list containing only SMILES e.g. smiles_only_lst
     counter = 0
     
-    data1.decode('utf-8', 'ignore')
+    data1 = data1.encode(encoding = 'UTF-8', errors = 'strict')
     
     descriptors = from_smiles(str(data1))
     df = pd.DataFrame(descriptors, index=[0])
