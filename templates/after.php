4eeaf28f0c62292abfb52f1116a4bf7e3ce820f1
Callmenicky <70570497+Callmenicky@users.noreply.github.com>
2021-11-14 14:31:24 +0800

    Update after.php

--- templates/after.php
+++ templates/after.php
@@ -53,7 +53,7 @@
 				<img src="{{url_for('static', filename='images/plots.PNG')}}" alt="identity icon"> 
 
 				{%else%}
-				<h1>InActive</h1>
+				<h1>Inactive</h1>
 				<img src="{{url_for('static', filename='images/plots.PNG')}}" alt="identity icon"> 
 				
 				{%endif%}
