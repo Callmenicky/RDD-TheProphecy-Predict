85ed7eecad40f55aa3b5334e16db14269170b3e9
Jing <34600547+ChinJingJie@users.noreply.github.com>
2021-11-16 20:36:13 +0800

    Update basicpred.php

--- templates/basicpred.php
+++ templates/basicpred.php
@@ -32,19 +32,11 @@
         <input type="text" id="smiles" name="smiles" placeholder="Enter smiles string" maxlength="150"/>
         <select name="disease" id="disease">
           <option value="default">Select Target Disease</option>
-          <?php
-                $conn = mysqli_connect('us-cdbr-east-04.cleardb.com', 'b0f9135aa66d86', '2e28f6a7', 'heroku_c703864e708562a');
-                $db = mysqli_select_db($conn, 'b0f9135aa66d86');
-
-                $query = " SELECT DISTINCT TargetDisease FROM model";
-                $query_run = mysqli_query($conn,$query);
-
-                while($row = mysqli_fetch_array($query_run)){
-           ?>
-                    <option><?php echo $row['TargetDisease']; ?></option>
-           <?php
-            }
-           ?>
+          {% for row in data %}    
+            <tr>
+                <option>{{row[0]}}</option>
+            </tr>
+        {% endfor %}
         </select>
         <p id="showmore"><a href="#" onclick="showMore()">more>></a></p>
         <div id="more">
