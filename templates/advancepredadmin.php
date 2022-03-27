d548bfd43eced609a78236136738f9588f880d3d
Callmenicky <70570497+Callmenicky@users.noreply.github.com>
2022-03-27 15:07:05 +0800

    Update advancepredadmin.php

--- templates/advancepredadmin.php
+++ templates/advancepredadmin.php
@@ -5,7 +5,7 @@
 
 <?php 
 $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-echo "<script>alert($actual_link);</script>"
+echo $actual_link;
 
 parse_str($url_components['query'], $params);
 
