5ed11386a2e5b3c327464f7898b37938f63520a2
Jing <34600547+ChinJingJie@users.noreply.github.com>
2022-03-28 23:06:49 +0800

    Update sessioncheckadmin.php

--- templates/sessioncheckadmin.php
+++ templates/sessioncheckadmin.php
@@ -1,14 +1,21 @@
 <?php
 session_start();
+$_SESSION['Email'] = "empty";
 $_SESSION['Email'] = $_GET['email'];
-if($_SESSION['Email']==""){
+if($_SESSION['Email']=="empty"){
 ?>
     <script type="text/javascript">
         alert("Login is required to proceed");
         window.location.href = "login.php";
     </script>
 <?php
-} 
+} else{
+?>
+    <script type="text/javascript">
+        alert($_SESSION['Email']);
+    </script>
+<?php
+}
 
 if($_SESSION['Admin']==0){
 ?>
